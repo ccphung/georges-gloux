4bc740964cb63242599777b8c488a4f3f8dffdab
Cédric Phung <cedric3011@outlook.fr>
2025-04-29 18:56:28 +0200

    modify header

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,26 +1,25 @@
-import logo from "../assets/images/logo.png";
-import header from "../assets/images/header.png";
-import Navbar from "../components/Navbar";
+import gg from "../assets/images/header.png";
 
-function Hero() {
+function Header() {
   return (
-    <>
-      <div className="flex justify-center items-center">
-        <div>
-          <img className="xl:w-[150px] w-20" src={logo} alt="logo" />
-        </div>
+    <div className="relative flex min-h-[80vh] flex-col items-center justify-start overflow-hidden bg-gray-200 px-10 py-10 md:min-h-[80vh]">
+      <div className="absolute inset-0 z-0 bg-gradient-to-b from-transparent via-orange-200 to-transparent opacity-50"></div>
 
-        <div className="relative xl:w-[30vw] w-[60vw]">
-          <img
-            className="relative z-10 w-full object-cover  brightness-75 hue-rotate-15"
-            src={header}
-            alt="header"
-          />
-        </div>
+      <div className="relative mt-5 flex flex-col items-center">
+        <h2 className="text-center text-5xl font-bold uppercase text-orange-500 opacity-70 md:text-[8vw]">
+          Georges Gloux
+        </h2>
       </div>
-      <Navbar />
-    </>
+
+      <div className="absolute bottom-0 z-10 xl:w-[25vw]">
+        <img
+          src={gg}
+          alt="Cédric"
+          className="w-full object-contain grayscale"
+        />
+      </div>
+    </div>
   );
 }
 
-export default Hero;
+export default Header;
